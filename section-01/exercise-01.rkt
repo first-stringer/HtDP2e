f8dcc3c0b8b7e74a0d01582deb9b0a1fdb79b6e1
first-stinger <justin.michael.stringer@gmail.com>
2017-01-24 05:46:43 -0600

    adding link

--- section-01/exercise-01.rkt
+++ section-01/exercise-01.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-01) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28counter._%28exercise._arith-n%29%29
+
 (define x 3)
 (define y 4)
 
