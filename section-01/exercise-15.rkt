e26033816d36573ff8cee33198b72c72cf039e3d
first-stinger <justin.michael.stringer@gmail.com>
2017-01-26 06:55:26 -0600

    adding section 1 exercise 15

---
+++ section-01/exercise-15.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-15) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3afuncs%29
+
+(define (==> sunny friday)
+  (or (not sunny) friday)
+  )
