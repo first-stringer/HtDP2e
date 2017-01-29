d3a8ab22c4e79f0a5d6499c08396130763f816e3
first-stinger <justin.michael.stringer@gmail.com>
2017-01-29 06:57:09 -0600

    adding section 1 exercise 31

---
+++ section-01/exercise-31.rkt
@@ -0,0 +1,39 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-31) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/batch-io)
+
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3aprogs%29
+
+;; Begin provided code.
+(define (letter fst lst signature-name)
+  (string-append
+    (opening fst)
+    "\n\n"
+    (body fst lst)
+    "\n\n"
+    (closing signature-name)))
+ 
+(define (opening fst)
+  (string-append "Dear " fst ","))
+ 
+(define (body fst lst)
+  (string-append
+   "We have discovered that all people with the" "\n"
+   "last name " lst " have won our lottery. So, " "\n"
+   fst ", " "hurry and pick up your prize."))
+ 
+(define (closing signature-name)
+  (string-append
+   "Sincerely,"
+   "\n\n"
+   signature-name
+   "\n"))
+
+(write-file 'stdout (letter "Matthew" "Fisler" "Felleisen"))
+;; End provided code.
+
+(write-file 'stdout (letter "Matt" "Fiser" "Feisen"))
+(write-file 'stdout (letter "Jay" "Springer" "Fiddletop"))
+(write-file 'stdout (letter "Milo" "Dee" "Fudpucker"))
+
