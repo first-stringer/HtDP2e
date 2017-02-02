e96412d94b07070dea82140d7ff266e967d01ab8
first-stinger <justin.michael.stringer@gmail.com>
2017-02-02 05:43:39 -0600

    adding section 1 exercise 40

---
+++ section-01/exercise-40.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-40) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._.D.K._sec~3adesign-world%29
+
+; WorldState -> WorldState 
+; moves the car by 3 pixels for every clock  tick
+; examples: 
+;   given: 20, expect 23
+;   given: 78, expect 81
+(define (tock ws)
+  (+ ws 3))
+
+(check-expect (tock 20) 23)
+(check-expect (tock 78) 81)
