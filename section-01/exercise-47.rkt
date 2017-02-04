349573e92746c8b417190ce74cd11ab504995a1c
first-stinger <justin.michael.stringer@gmail.com>
2017-02-04 13:37:11 -0600

    adding section 1 exercise 47, adding on key and main

--- section-01/exercise-47.rkt
+++ section-01/exercise-47.rkt
@@ -18,6 +18,9 @@
 (define SCENE_HEIGHT 100)
 (define SCENE (empty-scene SCENE_WIDTH SCENE_HEIGHT))
 
+(define DOWN_KEY_AMOUNT 6/5)
+(define UP_KEY_AMOUNT 4/3)
+
 ;; 2a. FUNCTION SIGNATURE: HappinessLevel -> HappinessLevel
 ;; 2b. PURPOSE STATEMENT: Decreases hl by RATE amount on every clock tick.
 ;; 2c. HEADER:
@@ -56,7 +59,11 @@
 ;;   (... hl ... MIN_HAPPINESS ... MAX_HAPPINESS ... SCENE_WIDTH...))
 ;; 5. CODE:
 (define (calc-gl hl)
-  (* (/ hl MAX_HAPPINESS) SCENE_WIDTH))
+  (if (< hl MAX_HAPPINESS)
+      (* (/ hl MAX_HAPPINESS) SCENE_WIDTH)
+      SCENE_WIDTH
+      )
+  )
 
 ;; 2a. FUNCTION SIGNATURE: HappinessLevel -> Image
 ;; 2b. PURPOSE STATEMENT: Displays the happiness level using a blank scene with a solid, red rectangle with a black frame.
@@ -64,15 +71,56 @@
 ;; (define (render hl) SCENE)
 ;; 3a. FUNCTIONAL EXAMPLES 7 TESTS:
 ;; Given: hl, Expect: (place-image (frame (rectangle (calc-gl hl) (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) SCENE))
-(check-expect (render 8) (place-image (frame (rectangle (calc-gl 8) (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) SCENE))
-(check-expect (render MIN_HAPPINESS) (place-image (frame (rectangle 0 (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) SCENE))
-(check-expect (render MAX_HAPPINESS) (place-image (frame (rectangle SCENE_WIDTH (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) SCENE))
+(check-expect (render 8) (place-image/align (frame (rectangle (calc-gl 8) (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) "left" "center" SCENE))
+(check-expect (render MIN_HAPPINESS) (place-image/align (frame (rectangle 0 (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) "left" "center" SCENE))
+(check-expect (render MAX_HAPPINESS) (place-image/align (frame (rectangle SCENE_WIDTH (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) "left" "center" SCENE))
 ;; 4. TEMPLATE:
 ;; (define (render hl)
 ;;  (... hl ... (calc-gl hl) ... SCENE_HEIGHT ... SCENE_WIDTH ... MAX_HAPPINESS...))
 ;; 5. CODE:
 (define (render hl)
-  (place-image (frame (rectangle (calc-gl hl) (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) SCENE)
+  (place-image/align (frame (rectangle (calc-gl hl) (ceiling (/ SCENE_HEIGHT 2)) "solid" "red")) 0 (ceiling (/ SCENE_HEIGHT 2)) "left" "center" SCENE)
   )
 
+;; 2a. FUNCTION SIGNATURE: HappinessLevel (hl) KeyEvent (ke) -> hl
+;; 2b. PURPOSE STATEMENT: Process keyboard events (ke). Every time the down
+;; arrow key is pressed, happiness increases by 1/5; every time the up arrow is
+;; pressed, happiness jumps by 1/3.
+;; 2c. HEADER:
+;; (define (handle-ke hl ke) hl)
+;; 3a. FUNCTIONAL EXAMPLES:
+;; Given: 10 "down", Expect: 12
+;; Given: 10 "up", Expect: 13
+;; 3b. TESTS:
+(check-expect (handle-ke 10 "down") 12)
+(check-expect (handle-ke 10 "up") 13)
+;; 4. TEMPLATE:
+;; (define (handle-ke hl ke)
+;;  (... hl ... ke ...))
+;; 5. CODE:
+(define (handle-ke hl ke)
+  (cond
+    [(key=? ke "down") (if (< (floor (* hl DOWN_KEY_AMOUNT)) MAX_HAPPINESS) (floor (* hl DOWN_KEY_AMOUNT)) MAX_HAPPINESS)]
+    [(key=? ke "up") (if (< (floor (* hl UP_KEY_AMOUNT)) MAX_HAPPINESS) (floor (* hl UP_KEY_AMOUNT)) MAX_HAPPINESS)]
+    [else hl]
+    )
+  )
 
+;; 2a. FUNCTION SIGNATURE: HappinessLevel (hl) -> hl
+;; 2b. PURPOSE STATEMENT: Initiates the happiness gauge with the maximum level
+;; of happiness.
+;; 2c. HEADER:
+;; (define (gauge-prog hl) hl)
+;; 3a. FUNCTIONAL EXAMPLES: NA
+;; 4. TEMPLATE:
+;; (define (gauge-prog hl)
+;;  (... hl ...))
+;; 5. CODE:
+(define (gauge-prog hl)
+  (big-bang hl
+            [on-tick tock]
+            [to-draw render]
+            [on-key handle-ke]
+            ))
+
+(gauge-prog MAX_HAPPINESS)
