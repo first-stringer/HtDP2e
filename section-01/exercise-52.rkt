69dc550e06c55466232e13b02b9e32fa0c486cce
first-stinger <justin.michael.stringer@gmail.com>
2017-02-06 16:40:39 -0600

    fixing formatting

--- section-01/exercise-52.rkt
+++ section-01/exercise-52.rkt
@@ -1,9 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-52) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3aintervals%29
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3aintervals%29
 
-3,4,5
-4,5
-3,4
-4
+;; 3,4,5
+;; 4,5
+;; 3,4
+;; 4
