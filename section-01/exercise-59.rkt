2e310767a260df73591e954a11cfa839de39c67c
first-stinger <justin.michael.stringer@gmail.com>
2017-02-08 06:22:26 -0600

    adding section 1, exercise 59

--- section-01/exercise-59.rkt
+++ section-01/exercise-59.rkt
@@ -10,7 +10,10 @@
 (define SCENE_HEIGHT 50)
 (define EMPTY_SCENE (empty-scene SCENE_WIDTH SCENE_HEIGHT))
 
-(define EMPTY_TL (place-image (rectangle (- SCENE_WIDTH 5) (- SCENE_HEIGHT 5) "outline" "black") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2) EMPTY_SCENE))
+(define EMPTY_TL (place-image (circle (/ SCENE_HEIGHT 4) "outline" "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2)
+                              (place-image (circle (/ SCENE_HEIGHT 4) "outline" "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2)
+                                           (place-image (circle (/ SCENE_HEIGHT 4) "outline" "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2)
+                                                        (place-image (rectangle (- SCENE_WIDTH 5) (- SCENE_HEIGHT 5) "outline" "black") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2) EMPTY_SCENE)))))
 
 ;; 1. DATA DEFINITIONS:
 ;; A TrafficLight is one of the following Strings:
@@ -19,7 +22,6 @@
 ;; – "yellow"
 ;; Interpretation: The three strings represent the three possible states that a
 ;; traffic light may assume.
-
 
 ;; 2a. FUNCTION SIGNATURE: TrafficLight -> TrafficLight
 ;; 2b. PURPOSE STATEMENT: Yields the next state given the current state (cs).
@@ -51,6 +53,26 @@
   )
 
 ;; 2a. FUNCTION SIGNATURE: TrafficLight -> Image
+;; 2b. PURPOSE STATEMENT: Creates the image of a one-color bulb.
+;; 2c. HEADER:
+;; (define (create-bulb color)
+;;   EMPTY_SCENE)
+;; 3a. FUNCTIONAL EXAMPLES:
+;; Given: "red", Expect: an activated red light bulb
+;; Given: "yellow", Expect: an activated yellow light bulb
+;; Given: "green", Expect: an activated green light bulb
+;; 3b. TESTS:
+(check-expect (create-bulb "red") (circle (/ SCENE_HEIGHT 4) "solid" "red"))
+(check-expect (create-bulb "yellow") (circle (/ SCENE_HEIGHT 4) "solid" "yellow"))
+(check-expect (create-bulb "green") (circle (/ SCENE_HEIGHT 4) "solid" "green"))
+;; 4. TEMPLATE:
+;; (define (create-bulb color)
+;;  (... color ...))
+;; 5. CODE:
+(define (create-bulb color)
+  (circle (/ SCENE_HEIGHT 4) "solid" color))
+  
+;; 2a. FUNCTION SIGNATURE: TrafficLight -> Image
 ;; 2b. PURPOSE STATEMENT: Renders the current state (cs) as an image.
 ;; 2c. HEADER:
 ;; (define (tl-render current-state)
@@ -60,21 +82,9 @@
 ;; Given: "yellow", Expect: tl with yellow light on
 ;; Given: "green, Expect: tl with green light on
 ;; 3b. TESTS:
-(check-expect (tl-render "red")
-              (place-image (circle (/ SCENE_HEIGHT 4) "solid" "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2)
-                           (place-image (circle (/ SCENE_HEIGHT 4) "outline" "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2)
-                                        (place-image (circle (/ SCENE_HEIGHT 4) "outline" "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2)
-                                                     EMPTY_TL))))
-(check-expect (tl-render "yellow")
-              (place-image (circle (/ SCENE_HEIGHT 4) "outline" "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2)
-                           (place-image (circle (/ SCENE_HEIGHT 4) "solid" "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2)
-                                        (place-image (circle (/ SCENE_HEIGHT 4) "outline" "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2)
-                                                     EMPTY_TL))))
-(check-expect (tl-render "green")
-              (place-image (circle (/ SCENE_HEIGHT 4) "outline" "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2)
-                           (place-image (circle (/ SCENE_HEIGHT 4) "outline" "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2)
-                                        (place-image (circle (/ SCENE_HEIGHT 4) "solid" "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2)
-                                                     EMPTY_TL))))
+(check-expect (tl-render "red") (place-image (create-bulb "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2) EMPTY_TL))
+(check-expect (tl-render "yellow") (place-image (create-bulb "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2) EMPTY_TL))
+(check-expect (tl-render "green") (place-image (create-bulb "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2) EMPTY_TL))
 ;; 4. TEMPLATE:
 ;; (define (tl-render cs)
 ;;   (cond
@@ -86,18 +96,9 @@
 ;; 5. CODE:
 (define (tl-render cs)
   (cond
-    [(string=? cs "red") (place-image (circle (/ SCENE_HEIGHT 4) "solid" "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2)
-                                      (place-image (circle (/ SCENE_HEIGHT 4) "outline" "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2)
-                                                   (place-image (circle (/ SCENE_HEIGHT 4) "outline" "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2)
-                                                                EMPTY_TL)))]
-    [(string=? cs "yellow") (place-image (circle (/ SCENE_HEIGHT 4) "outline" "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2)
-                                         (place-image (circle (/ SCENE_HEIGHT 4) "solid" "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2)
-                                                      (place-image (circle (/ SCENE_HEIGHT 4) "outline" "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2)
-                                                                   EMPTY_TL)))]
-    [(string=? cs "green") (place-image (circle (/ SCENE_HEIGHT 4) "outline" "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2)
-                                        (place-image (circle (/ SCENE_HEIGHT 4) "outline" "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2)
-                                                     (place-image (circle (/ SCENE_HEIGHT 4) "solid" "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2)
-                                                                  EMPTY_TL)))]
+    [(string=? cs "red") (place-image (create-bulb "red") (/ SCENE_WIDTH 4) (/ SCENE_HEIGHT 2) EMPTY_TL)]
+    [(string=? cs "yellow") (place-image (create-bulb "yellow") (/ SCENE_WIDTH 2) (/ SCENE_HEIGHT 2) EMPTY_TL)]
+    [(string=? cs "green") (place-image (create-bulb "green") (- SCENE_WIDTH (/ SCENE_WIDTH 4)) (/ SCENE_HEIGHT 2) EMPTY_TL)]
     )
   )
 
