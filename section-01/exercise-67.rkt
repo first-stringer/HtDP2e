57087d8fc01f7a3984c45477d52eed59f0cf303b
first-stinger <justin.michael.stringer@gmail.com>
2017-02-10 05:26:54 -0600

    adding section 1, exercise 67

---
+++ section-01/exercise-67.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-67) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3astructures%29
+
+(define SPEED 3)
+(define-struct balld [location direction])
+(make-balld 10 "up")
+
+(make-balld 7 "up")
+
+(make-balld 13 "down")
