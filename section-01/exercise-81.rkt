53e947dd43a4d8fccad370dfc0d9027a0a3232fc
first-stinger <justin.michael.stringer@gmail.com>
2017-02-14 05:28:41 -0600

    adding section 1, exercise 81

--- section-01/exercise-81.rkt
+++ section-01/exercise-81.rkt
@@ -4,18 +4,31 @@
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3adesignstructs%29
 
 ;; 1. DATA DEFINITIONS:
-;; A Time-point-since-midnight is a (make-time-point-since-midnight Number Number Number) where
+;; A Time-point is a (make-time-point Number Number Number) where
 ;; hours is an integer from 0 to 23,
 ;; minutes is an integer from 0 to 59, and
 ;; seconds is an integer from 0 to 59.
 ;; This DD copied from exercise number 77.
-(define-struct time-point-since-midnight [hours minutes seconds])
+(define-struct time-point [hours minutes seconds])
 
-;; 2a. FUNCTION SIGNATURE:
-;; 2b. PURPOSE STATEMENT:
+;; 2a. FUNCTION SIGNATURE: Time-point -> Number
+;; 2b. PURPOSE STATEMENT: Consumes instances of time-point (tp) structures and
+;; produces the number of seconds that have passed since midnight (hours * 60 * 60 + minutes * 60 + seconds).
 ;; 2c. HEADER:
+;; (define (time->seconds tp)
+;;   0)
 ;; 3a. FUNCTIONAL EXAMPLES:
-;; Given: , Expect:
+;; Given: 0 0 0, Expect: 0
+;; Given: 12 30 2, Expect: 45002
+;; Given: 23 59 59, Expect: 86399
 ;; 3b. TESTS:
+(check-expect (time->seconds (make-time-point 0 0 0)) 0)
+(check-expect (time->seconds (make-time-point 12 30 2)) 45002)
+(check-expect (time->seconds (make-time-point 23 59 59)) 86399)
 ;; 4. TEMPLATE:
+;; (define (time->seconds tp)
+;;   (... (time-point-hours tp) ... (time-point-minutes tp) ... (time-point-seconds tp) ...))
 ;; 5. CODE:
+(define (time->seconds tp)
+  (+ (* 3600 (time-point-hours tp)) (* 60 (time-point-minutes tp)) (time-point-seconds tp)))
+
