bbb43b07b1f0a3d8aaaa562bccd33b457b92ec0d
first-stinger <justin.michael.stringer@gmail.com>
2017-02-16 05:04:27 -0600

    adding section 1, exercise 84

--- section-01/exercise-84.rkt
+++ section-01/exercise-84.rkt
@@ -4,9 +4,11 @@
 (require 2htdp/universe)
 (require 2htdp/image)
 
+
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3aedit1%29
 
-;; 1a. DATA DEFINITIONS:
+
+;; 1a. DATA DEFINITIONS
 (define-struct editor [pre post])
 ;; An Editor is a structure:
 ;;   (make-editor String String)
@@ -14,23 +16,43 @@
 ;; (string-append s t) with the cursor displayed between s and t
 
 
-;; 1b. CONSTANT DEFINITIONS:
+;; 1b. CONSTANT DEFINITIONS
 (define EMPTY_SCENE (empty-scene 200 20))
 (define TEXT_SIZE 16)
 (define TEXT_COLOR "black")
 (define CURSOR (rectangle 1 20 "solid" "red"))
 
 
+;; 1c. FUNCTION WISH LIST
+;; NAME: move-last-pre-to-first-post 
+;; SIGNATURE: Editor -> Editor
+;; PURPOSE STATEMENT: Consumes an editor and removes the last character of pre
+;; and adds it to; the first position of post, unless pre is empty in which case
+;; editor is returned unchanged.
+;; NAME: move-first-post-to-last-pre
+;; SIGNATURE: Editor -> Editor
+;; PURPOSE STATEMENT: Consumes an editor and removes the first character of post
+;; and adds it to the last position of pre, unless post is empty in which case
+;; editor is returned unchanged.
+;; NAME: insert-char
+;; SIGNATURE: Editor 1String -> Editor
+;; PURPOSE STATEMENT: Consumes an editor and a 1String; the 1String is appended
+;; to pre.
+;; NAME: remove-char
+;; SIGNATURE: Editor -> Editor
+;; PURPOSE STATEMENT: Consumes an editor and removes the last character of pre.
+
+
 ;; 2a. FUNCTION SIGNATURE: Editor -> Image
 ;; 2b. PURPOSE STATEMENT: Which consumes an Editor (e) and produces an image.
-;; 2c. HEADER:
+;; 2c. HEADER
 ;; (define (render e) EMPTY_SCENE)
-;; 3a. FUNCTIONAL EXAMPLES:
+;; 3a. FUNCTIONAL EXAMPLES
 ;; Given: "hello" "world", Expect: image with CURSOR between the words hello and world
 ;; Given: "hello" "", Expect: image with CURSOR after the word hello
 ;; Given: "" "world", Expect: image with CURSOR before the word world
 ;; Given: "" "", Expect: image with a CURSOR on the left and nothing else
-;; 3b. TESTS:
+;; 3b. TESTS
 (check-expect (render (make-editor "hello" "world"))
               (overlay/align "left" "center"
                              (beside (text "hello" TEXT_SIZE TEXT_COLOR) CURSOR (text "world" TEXT_SIZE TEXT_COLOR))
@@ -47,21 +69,50 @@
               (overlay/align "left" "center"
                              (beside (text "" TEXT_SIZE TEXT_COLOR) CURSOR (text "" TEXT_SIZE TEXT_COLOR))
                              EMPTY_SCENE))
-;; 4. TEMPLATE:
+;; 4. TEMPLATE
 ;; (define (render e)
 ;;   (... (editor-pre e) ... (editor-post e) ...))
-;; 5. CODE:
+;; 5. CODE
 (define (render e) 
   (overlay/align "left" "center"
                  (beside (text (editor-pre e) TEXT_SIZE TEXT_COLOR) CURSOR (text (editor-post e) TEXT_SIZE TEXT_COLOR))
                  EMPTY_SCENE))
 
 
-;; 2a. FUNCTION SIGNATURE:
-;; 2b. PURPOSE STATEMENT:
-;; 2c. HEADER:
-;; 3a. FUNCTIONAL EXAMPLES:
-;; Given: , Expect:
-;; 3b. TESTS:
-;; 4. TEMPLATE:
-;; 5. CODE:
+;; 2a. FUNCTION SIGNATURE: Editor KeyEvent -> Editor 
+;; 2b. PURPOSE STATEMENT: Consumes two inputs, an Editor (e) and a KeyEvent (ke), and it produces another editor.
+;; 2c. HEADER
+;; (define (edit e ke) e)
+;; 3a. FUNCTIONAL EXAMPLES
+;; Given: "hello" "world" "\t", Expect: "hello" "world"
+;; Given: "hello" "" "\t", Expect: "hello" ""
+;; Given: "" "world" "\t", Expect: "" "world"
+;; Given: "hello" "world" "\r", Expect: "hello" "world"
+;; Given: "hello" "" "\r", Expect: "hello" ""
+;; Given: "" "world" "\r", Expect: "" "world"
+;; Given: "hello" "world" "\b", Expect: "hell" "world"
+;; Given: "" "world" "\b", Expect: "" "world"
+;; Given: "hello" "" "\b", Expect: "hell" ""
+;; Given: "hello" "world" "left", Expect: "hell" "oworld"
+;; Given: "hello" "" "left", Expect: "hell" "o"
+;; Given: "" "world" "left", Expect: "w" "orld"
+;; Given: "hello" "world" "right", Expect: "hellow" "orld"
+;; Given: "hello" "" "right", Expect: "hello" ""
+;; Given: "" "world" "right", Expect: "w" "orld"
+;; Given: "hello" "world" *', Expect: "hello" "world" ; *' = any key event not covered above
+;; 3b. TESTS
+;; 4. TEMPLATE
+;; (define (edit e ke)
+;;   (... (editor-pre e) ... (editor-post e) ... ke ...))
+;; 5. CODE
+(define (edit e ke)
+  (cond
+    [(string=? "\r" ke) e]
+    [(string=? "\t" ke) e]
+    ;[(string=? "\b" ke) (remove-char e)]
+    ;[(string=? "left" ke) (move-last-pre-to-first-post e)]
+    ;[(string=? "right" ke) (move-first-post-to-last-pre e)]
+    ;[(= (string-length ke) 1) (insert-char e ke)]
+    [else e]
+    )
+  )
