4d15a599624fc3964a92572bd040e6e5efb34a93
first-stinger <justin.michael.stringer@gmail.com>
2017-02-20 04:53:21 -0600

    adding section 1, exercise 88

---
+++ section-01/exercise-88.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-88) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3azoo2%29
+
+;; 1a. DATA DEFINITIONS
+(define-struct VCat [x hl])
+;; A VCat is a structure: (make-VCat Number Number)
+;; Interpretation: (make-VCat x hl) describes a virtual cat displayed on a
+;; one-dimensional axis at position x (a number) and its happiness level (a number
+;; from 0 to 100).
