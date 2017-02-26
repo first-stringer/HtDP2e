76dc9a7ca57c65318f2c600d53ade31ca05048fb
first-stinger <justin.michael.stringer@gmail.com>
2017-02-26 05:16:08 -0600

    adding section 1, exercise 94

---
+++ section-01/exercise-94.rkt
@@ -0,0 +1,47 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-94) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/universe)
+(require 2htdp/image)
+
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._ch~3amix%29
+
+
+;; 1a. DATA DEFINITIONS
+
+
+;; 1b. CONSTANT DEFINITIONS
+(define WIDTH 800)
+(define HEIGHT 150)
+(define EMPTY_SCENE (empty-scene WIDTH HEIGHT))
+(define SUN (overlay (circle 30 "solid" "yellow")
+                     (circle 35 "solid" "orange")
+                     (circle 38 "solid" "red")))
+(define TREE (above (overlay (circle 25 "solid" "yellowgreen")
+                             (circle 30 "solid" "green"))
+                    (rectangle 10 20 "solid" "brown")))
+(define UFO (overlay (circle 7 "solid" "red")
+                     (circle 10 "solid" "black")))
+(define BACKGROUND
+  (place-image TREE (- WIDTH 100) (- HEIGHT (/ (image-height TREE) 2))
+               (place-image SUN 45 45 EMPTY_SCENE)))
+
+(place-image UFO 100 100 BACKGROUND)
+
+
+
+;; 1c. FUNCTION WISH LIST
+;; NAME:
+;; SIGNATURE:
+;; PURPOSE STATEMENT:
+
+
+;; 2a. FUNCTION SIGNATURE:
+;; 2b. PURPOSE STATEMENT:
+;; 2c. HEADER
+;; 3a. FUNCTIONAL EXAMPLES
+;; #1: Given: , Expect:
+;; 3b. TESTS
+#;1 
+;; 4. TEMPLATE
+;; 5. CODE
