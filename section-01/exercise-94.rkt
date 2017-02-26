1b2cbe5f852b21e76b6a7716fde36c7a4dbad990
first-stinger <justin.michael.stringer@gmail.com>
2017-02-26 05:57:13 -0600

    adding section 1, exercise 94, adding data definitions

--- section-01/exercise-94.rkt
+++ section-01/exercise-94.rkt
@@ -8,6 +8,23 @@
 
 
 ;; 1a. DATA DEFINITIONS
+;; #1: A UFO is a Posn. 
+;; interpretation (make-posn x y) is the UFO's location 
+;; (using the top-down, left-to-right convention)
+ 
+(define-struct tank [loc vel])
+;; #2: A Tank is a structure:
+;;   (make-tank Number Number). 
+;; interpretation (make-tank x dx) specifies the position:
+;; (x, (- HEIGHT (/ (image-height TANK) 2))) and the tank's speed: dx pixels/tick 
+ 
+; #3: A Missile is a Posn. 
+; interpretation (make-posn x y) is the missile's place
+
+;; #4: A SIGS is one of: 
+;; – (make-aim UFO Tank)
+;; – (make-fired UFO Tank Missile)
+;; interpretation represents the complete state of a space invader game
 
 
 ;; 1b. CONSTANT DEFINITIONS
