cad9e115f0cc28bd98f81c4d1f13156fca4eb26f
first-stinger <justin.michael.stringer@gmail.com>
2017-03-12 18:07:01 -0500

    adding section 1, exercise 105

--- section-01/exercise-105.rkt
+++ section-01/exercise-105.rkt
@@ -1,3 +1,4 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-105) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._mix._sec~3aitemization-design2%29
