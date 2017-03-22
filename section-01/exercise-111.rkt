a50d2bab6fc4e521ce4b635c25167d7a7ac2934c
first-stinger <justin.michael.stringer@gmail.com>
2017-03-22 04:57:17 -0500

    adding section 1, exercise 111

---
+++ section-01/exercise-111.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-111) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3ainput-errors%29
+
+
+(define-struct vec [x y])
+; A vec is
+;   (make-vec PositiveNumber PositiveNumber)
+; interpretation represents a velocity vector
+
