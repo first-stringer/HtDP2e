627f55c2900fc11a773f1da5738edf1682a0c6d9
first-stinger <justin.michael.stringer@gmail.com>
2017-03-24 05:54:14 -0500

    adding section 1, exercise 120

---
+++ section-01/exercise-120.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-120) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3asynsem~3agrammar%29
+
+1. illegal, a single variable surrounded by parenthesis is not an expression
+
+2. legal but not grammatical (you cannot add 1 to a boolean).  (primitive
+   variable expression) where the expression is a legal functional application
+
+3. legal (primitive value value value)
+
