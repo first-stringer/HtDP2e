c29fe253c7445bcb65b85e372ff744432446cf08
first-stinger <justin.michael.stringer@gmail.com>
2017-03-24 05:42:27 -0500

    adding section 1, exercise 119

---
+++ section-01/exercise-119.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-119) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3asynsem~3agrammar%29
+
+1. cannot have a value in the inner set of parethesis of a function definition
+
+2. "(x)" should be an expression, a single variable surrounded by parenthesis is
+   not an expression
+
