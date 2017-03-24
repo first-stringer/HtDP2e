470c5ef8bda632d937f4f20f84a2f6bc752236b4
first-stinger <justin.michael.stringer@gmail.com>
2017-03-24 05:21:26 -0500

    adding section 1, exercise 116

---
+++ section-01/exercise-116.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-116) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3asynsem~3agrammar%29
+
+1. variable
+
+2. primitive followed by two variables, all enclosed in parenthesis
+
+3. primitive followed by expression and value, all enclosed in parenthesis
+
