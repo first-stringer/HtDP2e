ca19bbc5d3433a6500d423f9643c61469e6a78f5
first-stinger <justin.michael.stringer@gmail.com>
2017-03-24 05:26:32 -0500

    adding section 1, exercise 117

---
+++ section-01/exercise-117.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-117) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3asynsem~3agrammar%29
+
+1. cannot have a value followed by a primitive followed by a value (no infix
+   notation!)
+
+2. functional application must have at least one argument be enclosed in
+   parenthesis
+
+3. solitary variable cannot be enclosed in parenthesis
+
