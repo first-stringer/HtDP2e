a649c60ec20ef92b4d45a68ec16ac421f2916b4d
first-stinger <justin.michael.stringer@gmail.com>
2017-03-27 05:55:57 -0500

    adding section 1, exercise 123

--- section-01/exercise-123.rkt
+++ section-01/exercise-123.rkt
@@ -2,3 +2,13 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-123) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3asynsem~3aand-or%29
+
+#; (if exp-test exp-then exp-else)
+
+;; is short for
+
+#; (cond
+     [exp-test exp-then]
+     [else exp-else]
+     )
+
