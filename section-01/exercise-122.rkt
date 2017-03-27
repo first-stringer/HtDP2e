bc0a32c29129bd5d82ebe7fe67030b2e6f10645a
first-stinger <justin.michael.stringer@gmail.com>
2017-03-27 05:22:42 -0500

    adding section 1, exercise 122

--- section-01/exercise-122.rkt
+++ section-01/exercise-122.rkt
@@ -2,3 +2,19 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-122) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3asynsem~3asem%29
+
+(define (f x y)
+  (+ (* 3 x) (* y y)))
+
+;; 1.
+(+ (f 1 2) (f 2 1))
+(+ (+ (* 3 1) (* 2 2)) (f 2 1))
+(+ (+ 3 (* 2 2)) (f 2 1))
+(+ (+ 3 4) (f 2 1))
+(+ 7 (f 2 1))
+(+ 7 (+ (* 3 2) (* 1 1)))
+(+ 7 (+ 6 (* 1 1)))
+(+ 7 (+ 6 1))
+(+ 7 7)
+14
+
