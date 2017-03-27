8d57a88df519a35d87b591e024e39efd0de22175
first-stinger <justin.michael.stringer@gmail.com>
2017-03-27 06:09:55 -0500

    adding section 1, exercise 124

--- section-01/exercise-124.rkt
+++ section-01/exercise-124.rkt
@@ -2,3 +2,17 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-124) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3asynsem~3avar-defns~7d._%29
+
+;; 1.
+(define PRICE 5)
+#; (define SALES-TAX (* 0.08 PRICE))
+#; (define SALES-TAX (* 0.08 5))
+(define SALES-TAX 0.40)
+#; (define TOTAL (+ PRICE SALES-TAX))
+#; (define TOTAL (+ 5 0.40))
+(define TOTAL 5.40)
+
+;; 2. Yes because function fahrenheit->celsius is used before it is defined.
+
+;; 3. No because function f is defined before it is used.
+
