5686dccabb7c62c64260303853aceabbc4065aaa
first-stinger <justin.michael.stringer@gmail.com>
2017-03-28 05:18:07 -0500

    adding section 1, exercise 127

--- section-01/exercise-127.rkt
+++ section-01/exercise-127.rkt
@@ -4,17 +4,17 @@
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3aint1-structs%29
 
 
-(define-struct point [x y z])
-(define-struct none [])
+(define-struct ball [x y speed-x speed-y])
 
 
-;; 1. [1 2 3] Value.
+;; 1. (number? (make-ball 1 2 3 4)) = #false
 
-;; 2. [[1 2 3] 4 5] Value.
+;; 2. (ball-speed-y (make-ball (+ 1 2) (+ 3 3) 2 3)) = 3
 
-;; 3. [3 3 4] Value.
+;; 3. (ball-y (make-ball (+ 1 2) (+ 3 3) 2 3)) = 6
 
-;; 4. [] Not value.
+;; 4. (ball-x (make-posn 1 2)) = ERROR: "ball-x: expects a ball, given (make-posn
+;; 1 2)"
 
-;; 5. [1 4 5] Value.
+;; 5. (ball-speed-y 5) = ERROR: "ball-speed-y: expects a ball, given 5"
 
