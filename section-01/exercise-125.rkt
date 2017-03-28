ae15615a14054bce5cef4192a3aba22f5e97134b
first-stinger <justin.michael.stringer@gmail.com>
2017-03-28 04:50:51 -0500

    adding section 1, exercise 125

--- section-01/exercise-125.rkt
+++ section-01/exercise-125.rkt
@@ -3,5 +3,12 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-125) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3aint1-structs%29
 
-;; 1. 
- 
+;; 1. (define-struct oops []) = Illegal.  At least one variable name is required
+;; to be inside the brackets.
+
+;; 2. (define-struct child [parents dob date]) = Legal.
+
+;; 3. (define-struct (child person) [dob date]) = Illegal. The first term after
+;; the keyword define-struct must be a variable name and a list (two in this case)
+;; of parenthesis/brackets is not allowed.
+
