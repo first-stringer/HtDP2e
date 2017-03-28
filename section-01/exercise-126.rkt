eba0b1fdbe9719b8f579084f7063eab3db7750b9
first-stinger <justin.michael.stringer@gmail.com>
2017-03-28 05:05:59 -0500

    adding section 1, exercise 126

---
+++ section-01/exercise-126.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-126) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_one.html#%28part._sec~3aint1-structs%29
+
+
+(define-struct point [x y z])
+(define-struct none [])
+
+
+;; 1. [1 2 3] Value.
+
+;; 2. [[1 2 3] 4 5] Value.
+
+;; 3. [3 3 4] Value.
+
+;; 4. [] Not value.
+
+;; 5. [1 4 5] Value.
+
