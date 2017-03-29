1e7dda2fa1b5f820ce89e41b68a7bd6ea9069df9
first-stinger <justin.michael.stringer@gmail.com>
2017-03-29 05:35:21 -0500

    adding section 1, exercise 131

---
+++ section-02/exercise-131.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-131) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3alists-records%29
+
+
+;; A List-of-booleans is one of: 
+;; – '()
+;; – (cons Boolean List-of-booleans)
+;; interpretation all arbitrarily long lists of Booleans
+
