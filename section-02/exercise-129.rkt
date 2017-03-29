8619fd1c69f23bc0e45a37b3b293358bdb4d6993
first-stinger <justin.michael.stringer@gmail.com>
2017-03-29 05:07:49 -0500

    adding section 1, exercise 129

--- section-02/exercise-129.rkt
+++ section-02/exercise-129.rkt
@@ -1,5 +1,20 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-129) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._ch~3alists1%29
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3alists-records%29
+
+;; 1.
+(cons "Neptune"
+      (cons "Uranus"
+            (cons "Saturn"
+                  (cons "Jupiter"
+                        (cons "Mars"
+                              (cons "Earth"
+                                    (cons "Venus" (cons "Mercury" '()))))))))
+
+;; 2.
+(cons "mochi" (cons "sushi" (cons "miso soup" (cons "tsukemono" '()))))
+
+;; 3.
+(cons "white" (cons "red" (cons "yellow" (cons "blue" (cons "black" '())))))
 
