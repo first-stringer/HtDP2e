30bb44459b61664dbb55c1829e16032465224536
first-stinger <justin.michael.stringer@gmail.com>
2017-03-30 05:00:18 -0500

    adding section 1, exercise 135

--- section-02/exercise-135.rkt
+++ section-02/exercise-135.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-135) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3alists-programming%29
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3aeval-list%29
 
 
 ;; 1a. DATA DEFINITIONS
