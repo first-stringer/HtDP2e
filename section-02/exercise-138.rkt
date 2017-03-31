7c537f047988d48b716448de1bf6e9b2b03fab67
first-stinger <justin.michael.stringer@gmail.com>
2017-03-31 05:23:01 -0500

    adding section 1, exercise 138

---
+++ section-02/exercise-138.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-138) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3afinger-lists%29
+
+;;                        A List-of-amounts is one of: 
+;; – '()                        ^
+;;                              |
+;; – (cons PositiveNumber List-of-amounts)
+#; '() ; data definition example
+#; (cons 3 '()) ; data definition example
+#; (cons 5 (cons 3 '())) ; data definition example
+
+
+
+
+
+
