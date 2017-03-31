ba2452aaeaf48f8f74e4f2f429b93a00e6479558
first-stinger <justin.michael.stringer@gmail.com>
2017-03-31 05:38:17 -0500

    adding section 1, exercise 138

--- section-02/exercise-138.rkt
+++ section-02/exercise-138.rkt
@@ -3,6 +3,8 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-138) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3afinger-lists%29
 
+
+;; 1a. DATA DEFINITIONS
 ;;                        A List-of-amounts is one of: 
 ;; – '()                        ^
 ;;                              |
@@ -12,7 +14,24 @@
 #; (cons 5 (cons 3 '())) ; data definition example
 
 
-
+;; 2a. FUNCTION SIGNATURE: List-of-amounts -> Number
+;; 2b. PURPOSE STATEMENT: Consumes a List-of-amounts and computes the sum of the
+;; amounts.
+;; 2c. HEADER
+#; (define (sum loa) 0)
+;; 3a. FUNCTIONAL EXAMPLES & TESTS
+(check-expect (sum '()) 0)
+(check-expect (sum (cons 0 '())) 0)
+(check-expect (sum (cons 1 '())) 1)
+(check-expect (sum (cons 2 (cons 1 '()))) 3)
+;; 4. TEMPLATE
+(define (sum loa)
+  (cond
+    [(empty? loa) ...]
+    [else (... (first loa) ... (rest loa) ...)]
+    )
+  )
+;; 5. CODE
 
 
 
