728fa839d94424c916e7a145a6a0c257bc05348b
first-stinger <justin.michael.stringer@gmail.com>
2017-04-03 05:35:42 -0500

    adding section 1, exercise 139

--- section-02/exercise-139.rkt
+++ section-02/exercise-139.rkt
@@ -3,9 +3,51 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-139) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3afinger-lists%29
 
-;; A List-of-numbers is one of: 
+
+;; 1a. DATA DEFINITIONS
+;; #1:                  A List-of-amounts is one of: 
+;; – '()                        ^
+;;                              |
+;; – (cons PositiveNumber List-of-amounts)
+#; '() ; data definition example
+#; (cons 3 '()) ; data definition example
+#; (cons 5 (cons 3 '())) ; data definition example
+;; #2: A List-of-numbers is one of: 
 ;; – '()
 ;; – (cons Number List-of-numbers)
+#; (cons 5 '()) ; data definition example
+#; (cons -1 '()) ; data definition example
+#; (cons 5 (cons -1 '())) ; data definition example
+
+
+;; 1b. CONSTANT DEFINITIONS
+(define ERROR_MESSAGE "Cannot have negative amount!")
+
+
+;; 2a. FUNCTION SIGNATURE: List-of-amounts -> Number
+;; 2b. PURPOSE STATEMENT: Consumes a List-of-amounts and computes the sum of the
+;; amounts.
+;; 2c. HEADER
+#; (define (sum loa) 0)
+;; 3a. FUNCTIONAL EXAMPLES & TESTS
+(check-expect (sum '()) 0)
+(check-expect (sum (cons 0 '())) 0)
+(check-expect (sum (cons 1 '())) 1)
+(check-expect (sum (cons 2 (cons 1 '()))) 3)
+;; 4. TEMPLATE
+#; (define (sum loa)
+     (cond
+       [(empty? loa) ...]
+       [else (... (first loa) ... (sum (rest loa)) ...)]
+       )
+     )
+;; 5. CODE
+(define (sum loa)
+  (cond
+    [(empty? loa) 0]
+    [else (+ (first loa) (sum (rest loa)))]
+    )
+  )
 
 
 ;; 2a. FUNCTION SIGNATURE: List-of-numbers -> Boolean
@@ -34,5 +76,31 @@
     [(empty? lon) #true]
     [else (and (positive? (first lon)) (pos? (rest lon)))]
     ) 
+  )
+
+
+;; 2a. FUNCTION SIGNATURE: List-of-numbers -> Number
+;; 2b. PURPOSE STATEMENT: Consumes a List-of-numbers. It produces their sum if
+;; the input also belongs to List-of-amounts; otherwise it signals an error.
+;; 2c. HEADER
+#; (define (checked-sum lon) 0)
+;; 3a. FUNCTIONAL EXAMPLES & TESTS
+(check-expect (checked-sum '()) 0)
+(check-expect (checked-sum (cons 5 '())) 5)
+(check-expect (checked-sum (cons 6 (cons 5 '()))) 11)
+(check-error (checked-sum (cons -5 '())) ERROR_MESSAGE)
+;; 4. TEMPLATE
+#; (define (checked-sum lon)
+     (cond
+       [(pos? lon) (... lon ...)]
+       [else (... lon ...)]
+       )
+     )
+;; 5. CODE
+(define (checked-sum lon)
+  (cond
+    [(pos? lon) (sum lon)]
+    [else (error ERROR_MESSAGE)]
+    )
   )
 
