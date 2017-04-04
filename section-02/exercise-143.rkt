1aae4b6a91b2de6bb65866c46411d3693b1940c5
first-stinger <justin.michael.stringer@gmail.com>
2017-04-04 06:12:59 -0500

    adding section 1, exercise 143

--- section-02/exercise-143.rkt
+++ section-02/exercise-143.rkt
@@ -3,3 +3,41 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-143) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3alists~3ane%29
 
+
+;; 1a. DATA DEFINITIONS
+;; #1: A List-of-temperatures is one of: 
+;; – '()
+;; – (cons CTemperature List-of-temperatures)
+;; #2: A CTemperature is a Number greater than -273.
+
+
+;; 1b. CONSTANT DEFINITIONS
+
+
+;; 1c. FUNCTION WISH LIST
+;; #1 HEADER
+#; (define (sum alot) 0)
+;; SIGNATURE: List-of-temperatures -> Number 
+;; PURPOSE STATEMENT: Adds up the temperatures on the given list.
+;; #2 HEADER
+#; (define (how-many alot) 0)
+;; SIGNATURE: List-of-temperatures -> Number 
+;; PURPOSE STATEMENT: Counts the temperatures on the given list.
+
+
+;; 2a. FUNCTION SIGNATURE: List-of-temperatures -> Number
+;; 2b. PURPOSE STATEMENT: Computes the average temperature.
+;; 2c. HEADER
+#; (define (average alot) 0)
+;; 3a. FUNCTIONAL EXAMPLES & TESTS
+(check-expect (average (cons 1 (cons 2 (cons 3 '())))) 2)
+;; 4. TEMPLATE
+#; (define (average alot)
+     (cond
+       [(empty? alot) ...]
+       [(cons? alot)
+        (... (first alot) ...
+             ... (average (rest alot)) ...)]))
+;; 5. CODE
+(define (average alot)
+  (/ (sum alot) (how-many alot)))
