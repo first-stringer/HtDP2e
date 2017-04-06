e71d37557309a178f49a6720464f2309eb234ddb
first-stinger <justin.michael.stringer@gmail.com>
2017-04-06 05:40:30 -0500

    adding section 1, exercise 147

---
+++ section-02/exercise-147.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-147) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3alists~3ane%29
+
+
+;; 1a. DATA DEFINITIONS
+;; A NEList-of-Booleans is one of: 
+;; – (cons Boolean '())
+;; – (cons Boolean NEList-of-Booleans)
+;; interpretation non-empty lists of Booleans
+
