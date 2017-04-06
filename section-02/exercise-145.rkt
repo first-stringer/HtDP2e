46efb5f278d96912943559d9a4ccdaef226c0d17
first-stinger <justin.michael.stringer@gmail.com>
2017-04-06 05:00:59 -0500

    adding section 1, exercise 145

--- section-02/exercise-145.rkt
+++ section-02/exercise-145.rkt
@@ -3,3 +3,23 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-145) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3alists~3ane%29
 
+
+;; A NEList-of-temperatures is one of: 
+;; – (cons CTemperature '())
+;; – (cons CTemperature NEList-of-temperatures)
+;; interpretation non-empty lists of Celsius temperatures 
+
+
+;; 2a. FUNCTION SIGNATURE: NEList-of-temperatures -> Boolean
+;; 2b. PURPOSE STATEMENT: Consumes a NEList-of-temperatures. It produces #true if
+;; the temperatures are sorted in descending order, otherwise it produces #false.
+;; 2c. HEADER
+(define (sorted>? alot) #false)
+;; 3a. FUNCTIONAL EXAMPLES & TESTS
+(check-expect (sorted>? '()) #true)
+(check-expect (sorted>? (cons 0 '())) #true)
+(check-expect (sorted>? (cons 1 (cons 0 '()))) #true)
+(check-expect (sorted>? (cons 0 (cons 1 '()))) #false)
+;; 4. TEMPLATE
+;; 5. CODE
+
