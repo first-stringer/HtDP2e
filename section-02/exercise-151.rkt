f6b5aae64b23b779af5293641525f4f49b8df34a
first-stinger <justin.michael.stringer@gmail.com>
2017-04-10 05:09:36 -0500

    adding section 1, exercise 151

--- section-02/exercise-151.rkt
+++ section-02/exercise-151.rkt
@@ -3,3 +3,28 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-151) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._sec~3anats%29
 
+;; 1a. DATA DEFINITIONS
+;; A N is one of: 
+;; – 0
+;; – (add1 N)
+;; Interpretation: Represents the counting numbers.
+
+
+;; 2a. FUNCTION SIGNATURE: N -> Number
+;; 2b. PURPOSE STATEMENT: Computes (* n x) without using *.
+;; 2c. HEADER
+#; (define (multiply n x) 0)
+;; 3a. FUNCTIONAL EXAMPLES & TESTS
+(check-expect (multiply 0 0) 0)
+(check-expect (multiply 0 3) 0)
+(check-expect (multiply 3 0) 0)
+(check-expect (multiply 3 3) 9)
+;; 4. TEMPLATE
+#; (define (multiply n x)
+     (cond
+       [(zero? n) ...]
+       [else (... n ... (multiply ...) ...)]
+       )
+     )
+;; 5. CODE
+
