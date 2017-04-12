e3997de4d257273990e0a14aebb70393fba55a8b
first-stinger <justin.michael.stringer@gmail.com>
2017-04-12 05:52:37 -0500

    adding section 1, exercise 155

---
+++ section-02/exercise-155.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-155) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; http://www.ccs.neu.edu/home/matthias/HtDP2e/part_two.html#%28part._rd._sec~3ard%29
+
+
+;; 1a. DATA DEFINITIONS
+;; An RD (short for Russian doll) is one of: 
+;; – String 
+;; – (make-layer String RD)
+(define-struct layer [color doll])
+
+
+;; 2a. FUNCTION SIGNATURE:
+;; 2b. PURPOSE STATEMENT:
+;; 2c. HEADER
+;; 3a. FUNCTIONAL EXAMPLES
+;; #1: Given: , Expect:
+;; 3b. TESTS
+#;1 
+;; 4. TEMPLATE
+;; 5. CODE
+
